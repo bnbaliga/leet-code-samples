ed662e29ce95174206f79018845293da5cc5b6a6
Narasimha Baliga <bnbaliga@gmail.com>
2022-07-19 11:25:46 -0500

    inital commit

---
+++ Code/Console/ArrayProblemsMaxConsecutiveOnes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace LeetCodeSamplesConsole
+{
+    namespace LeetCodeSamples
+    {
+        public class ArrayProblemMaxConecutiveOnes
+        {
+            public int FindMaxConsecutiveOnes(int[] nums)
+            {
+
+                if (nums.Length < 1)
+                    return nums.Length;
+
+                if (nums.Length > (10 * 10 * 10 * 10 * 10))
+                    return nums.Length;
+
+                var consec = 0;
+                var lastKnownConsecCount = 0;
+                for (int i = 0; i <= nums.Length - 1; i++)
+                {
+                    if (nums[i] > 1)
+                    {
+                        lastKnownConsecCount = 0;
+                        break;
+                    }
+                    if (nums[i] == 1)
+                        consec++;
+                    if (nums[i] == 0)
+                    {
+                        if (consec > lastKnownConsecCount)
+                            lastKnownConsecCount = consec;
+                        consec = 0;
+                    }
+
+                }
+                if (consec > lastKnownConsecCount)
+                    lastKnownConsecCount = consec;
+                return lastKnownConsecCount;
+            }
+        }
+    }
+}
+
